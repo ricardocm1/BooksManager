7847d2157ed95d455ab722ec434a29733e380282
Ricardo Carra Marsilio <40778459+ricardocm1@users.noreply.github.com>
2020-02-17 11:54:35 -0300

    Code unused removed

--- app/src/main/java/com/android/booksmanager/modules/splash/SplashActivity.kt
+++ app/src/main/java/com/android/booksmanager/modules/splash/SplashActivity.kt
@@ -1,7 +1,6 @@
 package com.android.booksmanager.modules.splash
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.android.booksmanager.R
@@ -9,7 +8,6 @@ import com.android.booksmanager.extensions.postDelayed
 import com.android.booksmanager.modules.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
-    private val PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,19 +18,6 @@ class SplashActivity : AppCompatActivity() {
             startActivity(intent)
 
             finish()
-        }
-
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                }
-            }
         }
     }
 }
